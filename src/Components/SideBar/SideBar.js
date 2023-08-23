d04ac398b71e6c311695ff2d82212e408530b4f3
akulSingh18 <akulkumar.138@gmail.com>
2023-08-24 01:09:18 +0530

    tooltip added to sidebar

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -4,6 +4,7 @@ import jiraLogo from "../../Assets/jira-logo-png.png";
 import boardLogo from "../../Assets/board-logo.png";
 import leftArrow from "../../Assets/left-arrow.png";
 import rightArrow from "../../Assets/right-arrow.png";
+import plusSign from "../../Assets/plus-logo.png"
 
 
 
@@ -16,7 +17,7 @@ const SideBar = () => {
     let sideBar = document.getElementById("sidebar");
     let logoName = document.getElementById("logo-name");
 
-    if(activeSideBar == true){
+    if(activeSideBar === true){
       sideBar.style.width = "50px";
       logoName.style.display = "none"
       setActiveSideBar(false);
@@ -46,24 +47,102 @@ const SideBar = () => {
         <div  className="tab-add-cont">
           <div className="Tabs-cont">
             <div className="tabs">
-              <img className='board-logo' src={boardLogo} alt='B'/> 
-              <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Board 1</div>
+              <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 1
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
+             
+              <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Board 1
+              </div>
             </div>
             <div className="tabs">
-              <img className='board-logo' src={boardLogo} alt='B'/> 
+              <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 1
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
+             
+              <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Board 1
+              </div>
+            </div>
+            <div className="tabs">
+              <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 1
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
+             
+              <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Board 1
+              </div>
+            </div>
+            <div className="tabs">
+              <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 1
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
+             
+              <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Board 1
+              </div>
+            </div>
+            <div className="tabs">
+            <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 2
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
               <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"}>Board 2</div>
             </div>
             <div className="tabs">
-              <img className='board-logo' src={boardLogo} alt='B'/> 
+            <div className="tooltext-board">
+                <img className='board-logo' src={boardLogo} alt='B'/> 
+                {activeSideBar ?
+                null :
+                <span className='board-tooltext'>
+                  Board 3
+                </span>
+                }
+              </div>
+              {/* <img className='board-logo' src={boardLogo} alt='B'/>  */}
               <div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"}>Board 3</div>
             </div>
 
           </div>
           <div className="add-tab-cont">
             {activeSideBar ? (
-              <button className="primary-btn" >Add New Board</button>
+              <button className="primary-btn" ><img className='arrow' src={plusSign} alt='plus' />Add New Board</button>
             ):(
-              <button className="primary-btn" >+</button>
+              <button className="primary-btn"  >+
+              <span className='add-tool-text'>
+                Add Board
+              </span>
+              </button>
             )}
             
           </div>
@@ -72,10 +151,14 @@ const SideBar = () => {
         {/* ------------------------------------------------------------------------------------------------------------------------ */}
         <div className="hidebar-cont">
           {activeSideBar ? (
-            <button className="primary-btn" onClick={()=>toggleSideBar()} ><img className='arrow' src={leftArrow} alt='left' />add New Board</button>
+            <button className="primary-btn" onClick={()=>toggleSideBar()} ><img className='arrow' src={leftArrow} alt='left' /><div className={activeSideBar ? "active-tab-name" : "inactive-tab-name"} >Hide Side Bar</div></button>
           ):(
             
-            <button className="primary-btn-right-arrow" onClick={()=>toggleSideBar()} ><img className='arrow' src={rightArrow} alt='left' /></button>
+            <button className="primary-btn-right-arrow" onClick={()=>toggleSideBar()} ><img className='arrow' src={rightArrow} alt='left' />
+            <span className='tooltiptext'>
+              Show Side Bar.
+            </span>
+            </button>
             
           )}  
         </div>
