576151a5f1cda75622d289d588cfa953f2174c9b
Anshu-007 <jadon.anshu7@gmail.com>
2023-08-23 19:25:19 +0530

    board ui updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Board from './Components/Board/Board';
 import SideBar from './Components/SideBar/SideBar';
 
 function App() {
   return (
     <div className="App">
       <SideBar/>
+      <Board/>
 
       {/* rest of the work */}
     </div>
