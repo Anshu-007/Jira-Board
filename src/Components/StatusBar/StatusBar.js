a73ba413ae0caeda42642013b0df70aeceeca0e1
Anshu-007 <jadon.anshu7@gmail.com>
2024-09-28 00:30:22 +0530

    implemented edit feature / add create user feature

--- src/Components/StatusBar/StatusBar.js
+++ src/Components/StatusBar/StatusBar.js
@@ -24,48 +24,49 @@ const StatusBar = (props) => {
 
   const dispatch = useDispatch();
   
-  function handleDragStart(e){
-    e.stopPropagation();
-    setFromStatus(e.target.parentNode.id);
-    setTaskId(e.target.id)
-    // console.log('drag start',e.target.parentNode.id)
-  }
-  function handleDragOver(e){
-    e.stopPropagation();
-    e.preventDefault();
-    // console.log('drag over')
-  }
-  function handleDragEnd(e){
-    e.stopPropagation();
-    // console.log('drag ended')
-  }
+  // function handleDragStart(e){
+  //   e.stopPropagation();
+  //   setFromStatus(e.target.parentNode.id);
+  //   setTaskId(e.target.id)
+  //   // console.log('drag start',e.target.parentNode.id)
+  // }
+  // function handleDragOver(e){
+  //   e.stopPropagation();
+  //   e.preventDefault();
+  //   // console.log('drag over')
+  // }
+  // function handleDragEnd(e){
+  //   e.stopPropagation();
+  //   // console.log('drag ended')
+  // }
 
-  function handleDrop(e){
-    e.stopPropagation();
-    setToStatus(e.target.parentNode.id);
-    let boardId = getPath();
-    // console.log(boardId);
+  // function handleDrop(e){
+  //   e.stopPropagation();
+  //   setToStatus(e.target.parentNode.id);
+  //   let boardId = getPath();
+  //   // console.log(boardId);
    
-    // console.log(boardId);
-    let dropProperties = {
-      fromStatus : fromStatus,
-      toStatus : e.target.parentNode.id,
-      boardId :  boardId,
-      taskId : taskId
-    }
-    // console.log(dropProperties);
-    dispatch(dragAndDrop(dropProperties));
+  //   // console.log(boardId);
+  //   let dropProperties = {
+  //     fromStatus : fromStatus,
+  //     toStatus : e.target.parentNode.id,
+  //     boardId :  boardId,
+  //     taskId : taskId
+  //   }
+  //   // console.log(dropProperties);
+  //   dispatch(dragAndDrop(dropProperties));
     
-    // console.log('drag drop',fromStatus,e.target.parentNode.id)
-  }
+  //   // console.log('drag drop',fromStatus,e.target.parentNode.id)
+  // }
   return (
     <div 
       key={index}
+
       className='status-bar'
-      onDragStart={handleDragStart}
-      onDragOver={handleDragOver} 
-      onDragEnd={handleDragEnd}
-      onDrop={handleDrop}
+      // onDragStart={handleDragStart}
+      // onDragOver={handleDragOver} 
+      // onDragEnd={handleDragEnd}
+      // onDrop={handleDrop}
       id={StatusBarHeading}
     >
         <div className='status-heading'>{StatusBarHeading}</div>
